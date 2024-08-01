481aa7ad1f1eaa4b6e6c950c2dd26f91eb5bf791
Sergey <sergey@gmail.com>
2024-08-01 18:47:16 +0300

    changes done to first quest: modified logic, optimisation

--- postgresql/First_quest.py
+++ postgresql/First_quest.py
@@ -2,28 +2,28 @@ import psycopg2
 from config import *
 
 
-def fill_cat_colors(cursor):
-    cursor.execute("DELETE FROM cat_colors_info")
-    cursor.execute("SELECT color, COUNT(*) as count FROM cats GROUP BY color")
-    color_count = cursor.fetchall()
-    return color_count
-
-
-def insert_into_database(cursor, count: tuple):
-    for color, num in count:
-        cursor.execute(
-            "INSERT INTO cat_colors_info (color, count) VALUES (%s, %s)", (color, num))
-
-
 def main():
     try:
+        # Connect to the PostgreSQL database
         with psycopg2.connect(database=database, user=user, password=password, port=port) as connection:
             with connection.cursor() as cursor:
+                # Fetch the PostgreSQL version
                 cursor.execute('SELECT version()')
+                version = cursor.fetchone()
+                print(f"PostgreSQL version: {version[0]}")
 
-            with connection.cursor() as cursor:
-                count = fill_cat_colors(cursor)
-                insert_into_database(cursor, count)
+                # Delete existing records from cat_colors_info
+                cursor.execute("DELETE FROM cat_colors_info")
+
+                # Insert color count data into cat_colors_info
+                cursor.execute("""
+                    INSERT INTO cat_colors_info (color, count)
+                    SELECT color, COUNT(*)
+                    FROM cats
+                    GROUP BY color
+                """)
+
+                # Commit the transaction
                 connection.commit()
 
     except psycopg2.Error as e:
@@ -33,6 +33,7 @@ def main():
         if connection:
             connection.close()
             print('PostgreSQL connection is closed')
+
 
 if __name__ == '__main__':
     main()
