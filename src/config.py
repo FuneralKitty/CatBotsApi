835f33871f75f339b319ba235fcc7b19bb594714
mac <mac@MacBook-Pro-mac.local>
2024-08-12 10:45:51 +0300

    changes

--- src/config.py
+++ src/config.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-DB_CONFIG = {
-    'dbname': 'wg_forge_db',
-    'user': 'wg_forge',
-    'password': '42a',
-    'host': '192.168.0.111',
-    'port': '5432'
-}
-=======
 import os
 
 DB_CONFIG = {
