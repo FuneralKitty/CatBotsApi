a41148195304dfbf428f1cdec7e767d8fbfbf6ab
mac <risephoenixpuryfire@gmail.com>
2024-08-17 10:35:47 +0300

    DEV-55 <message>

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+from typing import Optional, Any, Tuple, Dict
 import psycopg
+from psycopg_pool import ConnectionPool
 from src.config import DB_CONFIG
-from src.data_fullfilling import cat_colors_create_data, fullfill_cat_options
-from flask import Flask, request, jsonify
+from src.data_fullfilling import cat_colors_create_data, fullfill_cat_options, add_info_db, get_parsed_data
+from flask import Flask, request, jsonify, Response
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 
+pool = ConnectionPool(conninfo=f"dbname={DB_CONFIG['dbname']} user={DB_CONFIG['user']} password={DB_CONFIG['password']} host={DB_CONFIG['host']} port={DB_CONFIG['port']}")
+
+app_name = "Cats Service"
 app = Flask(__name__)
 limiter = Limiter(
     app=app,
@@ -13,56 +18,27 @@ limiter = Limiter(
     default_limits=["600 per minute"]
 )
 
-valid_attributes = ['name', 'color', 'tail_length', 'whiskers_length']
-
 
 @app.route('/ping', methods=['GET'])
-def ping():
-    return "Cats Service. Version 0.1", 200
+def ping() -> Tuple[str, int]:
+    return f"{app_name}. Version 0.1", 200
 
 
 @app.route('/cats', methods=['GET'])
 @limiter.limit("600 per minute")
-def data_parser():
-    conn = None
+def data_parser() -> tuple[Response, int]:
     attribute = request.args.get("attribute", default='name')
     order = request.args.get("order", default='asc')
     offset = request.args.get("offset", default=0, type=int)
     limit = request.args.get("limit", default=10, type=int)
 
-    if attribute not in valid_attributes:
-        return jsonify({'error': 'Invalid attribute'}), 400
-    if order not in ['asc', 'desc']:
-        return jsonify({'error': 'Invalid order'}), 400
-
-    db_request = f"""
-                SELECT name, color, tail_length, whiskers_length
-                FROM cats
-                ORDER BY {attribute} {order}
-                OFFSET %s LIMIT %s
-                """
-    try:
-        conn = psycopg.connect(**DB_CONFIG)
-        with conn.cursor() as cur:
-            cur.execute(db_request, (offset, limit))
-            cats = cur.fetchall()
-            if not cats:
-                return jsonify({'error': 'No cats found'}), 404
-
-            data = [
-                {'name': cat[0], 'color': cat[1], 'tail_length': cat[2], 'whiskers_length': cat[3]}
-                for cat in cats
-            ]
-            return jsonify(data), 200
-    except Exception as e:
-        app.logger.error(f"Error occurred: {str(e)}")
-        return jsonify({'error': str(e)}), 500
-    finally:
-        if conn:
-            conn.close()
+    result: Tuple[Dict[str, str], int] = get_parsed_data(pool, attribute, order, offset, limit)
+    return jsonify(result[0]), result[1]
 
 
-def validate_attributes(data):
+
+def validate_attributes(data) -> tuple[Response, int] | None:
+    # FIXMI: возможно сделать через pydantic
     errors = []
     if 'name' not in data or not isinstance(data['name'], str):
         errors.append('Name is invalid')
@@ -82,36 +58,14 @@ def validate_attributes(data):
 
 
 @app.route('/cat', methods=['POST'])
-def add_info():
+def add_info() -> tuple[dict[str, str], int] | tuple[Response, int]:
     data = request.get_json()
-    conn = None
     validation_response = validate_attributes(data)
     if validation_response:
         return validation_response
 
-    try:
-        conn = psycopg.connect(**DB_CONFIG)
-        with conn.cursor() as cur:
-            cur.execute(
-                "SELECT name FROM cats WHERE name = %s", (data['name'],))
-            existing_cat = cur.fetchone()
-
-            if existing_cat:
-                return jsonify({'error': 'Cat already exists'}), 409
-
-            cur.execute("""
-                INSERT INTO cats (name, color, tail_length, whiskers_length)
-                VALUES (%s, %s, %s, %s)
-            """, (data['name'], data['color'], data['tail_length'], data['whiskers_length']))
-            conn.commit()
-
-            return jsonify({'message': 'Cat added successfully'}), 201
-    except Exception as e:
-        app.logger.error(f"Error occurred: {str(e)}")
-        return jsonify({'error': str(e)}), 500
-    finally:
-        if conn:
-            conn.close()
+    result: Tuple[Dict[str, Any], int] = add_info_db(pool, data)
+    return jsonify(result[0]), result[1]
 
 
 if __name__ == '__main__':
